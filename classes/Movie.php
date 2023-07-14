257a1a8c95d2a669e783286a0cb8b12e3b4fdacf
rc2pc2 <petriccarcc@gmail.com>
2023-07-14 10:03:20 +0200

    added php doc to the Movie class

--- classes/Movie.php
+++ classes/Movie.php
@@ -1,5 +1,11 @@
 <?php
-
+    /**
+     * This class is an example of a Movie.
+     * @license Boolean
+     * @version 1.0.0
+     * @author Riccardo Petricca <rcpc.bool@gmail.com>
+     *
+     * */
     class Movie{
         public $title;
         public $director;
@@ -7,6 +13,15 @@
         public $year;
         public $genres;
 
+        /**
+         * Consctructor for a new Movie, genres are optional.
+         *
+         * @param String $_title the title of the movie.
+         * @param String $_director the director of the movie.
+         * @param String $_synopsis the synopsis of the movie.
+         * @param Int $_year the year in which the movie has been released.
+         * @param array $_genres optional: the list of genres of this movie.
+         */
         function __construct( String $_title,  String $_director, String $_synopsis, Int $_year, Array $_genres = [] ){
             $this->title = $_title;
             $this->director = $_director;
@@ -15,6 +30,10 @@
             $this->genres = $_genres;
         }
 
+        /**
+         * Returns the movie's title.
+         *
+         */
         public function getTitle(){
             return $this->title;
         }
